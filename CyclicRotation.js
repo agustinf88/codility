3537daa03ee51fe2fcdddd3fe62c86fa40bca06c
HSNBDES32\afiorenza <agustin.fiorenza@hasar.com>
2017-11-29 10:49:19 -0300

    Agrego CyclicRotation

--- CyclicRotation.js
+++ CyclicRotation.js
@@ -1,6 +1,23 @@
-function solution(A, K){
-
-  console.log('CyclicRotation');
+function solution(A, K) {
+  if (A.length === K) {
+    return A;
+  }
+  var l;
+  if (K > A.length) {
+    if (K % A.length === 0) {
+      return A;
+    } else {
+      const aRestar = ~~(K / A.length) * A.length;
+      K = K - aRestar;
+    }
+  }
+  l = A.length - K;
+  const removed = A.splice(0, l);
+  return [...A, ...removed];
 }
 
-solution(1,1);
+const list = [3, 8, 9, 7, 6];
+console.log("Array:    ", list);
+console.log("Solución: ", solution(list, 101));
+
+// console.log(~~(32 / 10));
