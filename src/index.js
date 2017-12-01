206f440f503c65b498a66566269a316a83333b42
HSNBDES32\afiorenza <agustin.fiorenza@hasar.com>
2017-12-01 11:59:14 -0300

    MissingInteger

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 require("./L1/BinaryGap")(20);
 require("./L4/FrogRiverOne")(5, [1, 3, 1, 4, 2, 3, 5, 4]);
+require("./L4/MissingInteger")([1, 6, 4, 1, 2, 7, 8, 9, 10]);
+require("./L4/MissingInteger")([-1, -3]);
