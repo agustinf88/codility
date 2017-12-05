fd1d33a4fc0332391f6e5a187cb09c7b470cdf26
HSNBDES32\afiorenza <agustin.fiorenza@hasar.com>
2017-12-05 12:11:22 -0300

    Agrego archivos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 require("./L1/BinaryGap")(20);
 require("./L3/TapeEquilibrium")([-100, 100]);
-require("./L3/TapeEquilibrium")([3, 1, 2, 4, 3,]);
+require("./L3/TapeEquilibrium")([3, 1, 2, 4, 3]);
 require("./L4/FrogRiverOne")(5, [1, 3, 1, 4, 2, 3, 5, 4]);
 require("./L4/MissingInteger")([1, 6, 4, 1, 2, 7, 8, 9, 10]);
 require("./L4/MissingInteger")([-1, -3]);
-require("./L5/CountDiv")(0, 1, 2);
 require("./L4/MaxCounters")(5, [1, 2, 2, 2, 6, 2]);
 require("./L4/MaxCounters")(5, [3, 4, 4, 6, 1, 4, 4]);
+require("./L5/CountDiv")(0, 1, 2);
+require("./L5/PassingCars")([0, 1, 0, 1, 1]);
+require("./L5/PassingCars")([0,0,0,0,0,1]);
+require("./L5/PassingCars")([1]);
