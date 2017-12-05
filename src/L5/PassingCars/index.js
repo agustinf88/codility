807de94928caf9662be791cfe3dacfa6f268e2c9
HSNBDES32\afiorenza <agustin.fiorenza@hasar.com>
2017-12-05 11:55:07 -0300

    Agrego Passing Cars

---
+++ src/L5/PassingCars/index.js
@@ -0,0 +1,22 @@
+function solution(A) {
+  let count = 0;
+  let m = 0;
+
+  for (var i = 0; i < A.length; i++) {
+    if (A[i] === 0) {
+      m++;
+    }
+    if (m > 0 && A[i] === 1) {
+      count += m;
+      if (count > 1000000000) {
+        return -1;
+      }
+    }
+  }
+  return count;
+}
+
+module.exports = list =>
+  console.log(
+    `** PassingCars ** - list: ${list} - solution: ${solution(list)}`
+  );
