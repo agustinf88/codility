fecfddb8aac82c69873bc7dcbcefd2d8a917565d
HSNBDES32\afiorenza <agustin.fiorenza@hasar.com>
2017-12-08 15:13:06 -0300

    Agrego L6/Distinct

---
+++ src/L6/Distinct/index.js
@@ -0,0 +1,17 @@
+function solution(A) {
+  let counter = {};
+
+  for (var i = 0; i < A.length; i++) {
+    console.log("Counter: ", counter);
+    if (!counter[A[i]]) {
+      counter = Object.assign(counter, { [A[i]]: true });
+    }
+  }
+
+  return Object.keys(counter).length;
+}
+
+module.exports = list =>
+  console.log(
+    `** Distinct       ** - List:${list} - solution: ${solution(list)}`
+  );
