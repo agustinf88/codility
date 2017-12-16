e5f22c02b6ca3831a017dc000ff599891f6d3bbf
HSNBDES32\afiorenza <agustin.fiorenza@hasar.com>
2017-12-16 15:45:15 -0300

    Matemático..

---
+++ src/L8/EquiLeader/index.js
@@ -0,0 +1,16 @@
+function solution(A) {
+  let counter = [
+    {
+      indes: 0,
+      left: { value: undefined, count: undefined, length: undefined },
+      rigth: { value: undefined, count: undefined, length: undefined }
+    }
+  ];
+  return "HOLA";
+}
+
+let list = [4, 3, 4, 4, 4, 2];
+
+console.log(
+  `** Equilider       ** - list: ${list} - solution: ${solution(list)}`
+);
